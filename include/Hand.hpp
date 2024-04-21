159e6f9dcf5fcb19744824014fa323e485bf9254
Gary Hilares <dev.garyhilares@gmail.com>
2024-04-21 03:24:48 -0700

    Refactored Hand methods into their own file

--- include/Hand.hpp
+++ include/Hand.hpp
@@ -12,73 +12,13 @@ private:
     Card hand[MAX_HAND_SIZE] = { { "", 0 } };
 
 public:
-    void draw(bool is_opponent = false)
-    {
-        for (int i = 0; i < MAX_HAND_SIZE; i++) {
-            if (hand[i].number == 0) {
-                hand[i] = getRandomCard();
-                if (!is_opponent) {
-                    std::cout << "Drew " << hand[i].number << " of " << hand[i].pale << "." << std::endl;
-                } else {
-                    std::cout << "Your opponent drew a card." << std::endl;
-                }
-            }
-        }
-    }
-    int getSize() const
-    {
-        int number_of_cards = 0;
-        for (int i = 0; i < MAX_HAND_SIZE; i++) {
-            if (hand[i].number != 0) {
-                number_of_cards++;
-            }
-        }
-        return number_of_cards;
-    }
-    void print() const
-    {
-        std::cout << "Hand:" << std::endl;
-        for (int i = 0; i < MAX_HAND_SIZE; i++) {
-            if (hand[i].number != 0) {
-                std::cout << hand[i].number << " of " << hand[i].pale << std::endl;
-            }
-        }
-    }
-    void discard(int index, bool is_oponent = false)
-    {
-        if (!is_oponent) {
-            std::cout << "Discarded " << hand[index].number << " of " << hand[index].pale << "." << std::endl;
-        } else {
-            std::cout << "Your opponent discarded a " << hand[index].number << " of " << hand[index].pale << "." << std::endl;
-        }
-        hand[index] = { "", 0 };
-    }
-    bool isStraight() const
-    {
-        Card temporal_hand[MAX_HAND_SIZE] = { { "", 0 } };
-        for (int i = 0; i < MAX_HAND_SIZE; i++) {
-            temporal_hand[i] = hand[i];
-        }
-        std::sort(std::begin(temporal_hand), std::end(temporal_hand));
-        for (int i = 2; i < MAX_HAND_SIZE; i++) {
-            if (temporal_hand[i].number != temporal_hand[i - 1].number + 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-    void discardAllByNumber(int number, bool is_oponent = false)
-    {
-        for (int i = 0; i < MAX_HAND_SIZE; i++) {
-            if (hand[i].number == number) {
-                discard(i, is_oponent);
-            }
-        }
-    }
-    Card* getArray()
-    {
-        return hand;
-    }
+    void draw(bool is_opponent = false);
+    int getSize() const;
+    void print() const;
+    void discard(int index, bool is_oponent = false);
+    bool isStraight() const;
+    void discardAllByNumber(int number, bool is_oponent = false);
+    Card* getArray();
 };
 
 #endif // HAND_HPP
---
+++ src/Hand.cpp
@@ -0,0 +1,75 @@
+#include "../include/Hand.hpp"
+
+void Hand::draw(bool is_opponent)
+{
+    for (int i = 0; i < MAX_HAND_SIZE; i++) {
+        if (hand[i].number == 0) {
+            hand[i] = getRandomCard();
+            if (!is_opponent) {
+                std::cout << "Drew " << hand[i].number << " of " << hand[i].pale << "." << std::endl;
+            } else {
+                std::cout << "Your opponent drew a card." << std::endl;
+            }
+        }
+    }
+}
+
+int Hand::getSize() const
+{
+    int number_of_cards = 0;
+    for (int i = 0; i < MAX_HAND_SIZE; i++) {
+        if (hand[i].number != 0) {
+            number_of_cards++;
+        }
+    }
+    return number_of_cards;
+}
+
+void Hand::print() const
+{
+    std::cout << "Hand:" << std::endl;
+    for (int i = 0; i < MAX_HAND_SIZE; i++) {
+        if (hand[i].number != 0) {
+            std::cout << hand[i].number << " of " << hand[i].pale << std::endl;
+        }
+    }
+}
+
+void Hand::discard(int index, bool is_oponent)
+{
+    if (!is_oponent) {
+        std::cout << "Discarded " << hand[index].number << " of " << hand[index].pale << "." << std::endl;
+    } else {
+        std::cout << "Your opponent discarded a " << hand[index].number << " of " << hand[index].pale << "." << std::endl;
+    }
+    hand[index] = { "", 0 };
+}
+
+bool Hand::isStraight() const
+{
+    Card temporal_hand[MAX_HAND_SIZE] = { { "", 0 } };
+    for (int i = 0; i < MAX_HAND_SIZE; i++) {
+        temporal_hand[i] = hand[i];
+    }
+    std::sort(std::begin(temporal_hand), std::end(temporal_hand));
+    for (int i = 2; i < MAX_HAND_SIZE; i++) {
+        if (temporal_hand[i].number != temporal_hand[i - 1].number + 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Hand::discardAllByNumber(int number, bool is_oponent)
+{
+    for (int i = 0; i < MAX_HAND_SIZE; i++) {
+        if (hand[i].number == number) {
+            discard(i, is_oponent);
+        }
+    }
+}
+
+Card* Hand::getArray()
+{
+    return hand;
+}
